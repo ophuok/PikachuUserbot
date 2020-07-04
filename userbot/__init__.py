b681bcc624b999ec86be2132fb6598021233fab4
Sachin Jangir <sachinjangir022@gmail.com>
2020-07-04 15:20:47 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -9,7 +9,7 @@ from telethon import TelegramClient
 
 from var import Var
 
-os.system("pip install --upgrade pip")
+os.system("os.system("pip install --upgrade pip && python -m spacy download en_core_web_sm")")
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
     bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
