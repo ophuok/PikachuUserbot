bc8bf8a7ed902f98005deb0f558b42819cd8fdc6
Sachin Jangir <sachinjangir022@gmail.com>
2020-07-22 22:22:12 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,16 +9,16 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-ALIVE_IMG = "https://telegra.ph/file/5fbacbbc038bb7fac6704.jpg"
-ALIVE_caption = "`Pikachu Is:` **𝑶𝒏𝒍𝒊𝒏𝒆**\n\n"
-ALIVE_caption += "**System Status **\n\n"
-ALIVE_caption += "`Telethon Version:` **6.0.9**\n`Python:` **3.7.4**\n\n"
-ALIVE_caption += "`Data Servers :` **Functioning**\n\n"
-ALIVE_caption += "**Current Branch** : `master`\n\n"
-ALIVE_caption += "**PikaOS** : `1.0 Beta`\n\n"
-ALIVE_caption += f"**My Peru Owner** : {DEFAULTUSER} \n\n"
-ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-ALIVE_caption += "Copyright By [ItzSjDude](t.me/ItzSjDude)\n\n"
+ALIVE_IMG = "https://telegra.ph/file/a34493e9d843193edb7d5.jpg"
+ALIVE_caption = "`Pikachu Is:` **𝑶𝒏𝒍𝒊𝒏𝒆**\n"
+ALIVE_caption += "**System Status **\n"
+ALIVE_caption += "`Telethon Version:` **6.0.9**\n`Python:` **3.7.4**\n"
+ALIVE_caption += "`Data Servers :` **Functioning**\n"
+ALIVE_caption += "**Current Branch** : `master`\n"
+ALIVE_caption += "**PikaOS** : `1.0 Beta`\n"
+ALIVE_caption += f"**My Peru Owner** : {DEFAULTUSER} \n"
+ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n"
+ALIVE_caption += "Copyright By [ItzSjDude](t.me/ItzSjDude)\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
