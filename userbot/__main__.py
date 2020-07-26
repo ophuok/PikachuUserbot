2d18464f19784d3312ab0de5b6de21626c9cb557
Sachin Jangir <sachinjangir022@gmail.com>
2020-07-26 18:10:12 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -58,10 +58,6 @@ async def install(event):
     for ixo in total_doxx:
         mxo = documentss[ixo].id
         downloaded_file_name = await event.client.download_media(await borg.get_messages(chat, ids=mxo), "userbot")
-        if "(" not in downloaded_file_name:
-            path1 = Path(downloaded_file_name)
-            shortname = path1.stem
-            load_module(shortname.replace(".py", "")
 
 print("Imported Plugins Database Sucessfully")
 
