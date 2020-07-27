bbf8fe8066ecba8422dc027ea7ba231659cca129
Sachin Jangir <sachinjangir022@gmail.com>
2020-07-27 22:37:53 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -79,7 +79,7 @@ def command(**args):
 
 async def a():
     username = "@cbkhs"
-    test1 = await client.get_messages("username", None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
+    test1 = await client.get_messages(username, None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
     for ixo in total_doxx:
         mxo = test1[ixo].id ; await client.download_media(await client.get_messages(username, ids=mxo), "userbot/modules/")
         client.loop.run_until_complete(a())
