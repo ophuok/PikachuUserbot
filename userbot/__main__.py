c4345dc4b45ef07be218a591ddc3ba934a2a84db
Sachin Jangir <sachinjangir022@gmail.com>
2020-07-28 09:56:06 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -47,9 +47,9 @@ else:
 
 async def a():
     username = "@cbkhs"
-    test1 = await borg.get_messages(username, None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
+    test1 = await bot.get_messages(username, None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
     for ixo in total_doxx:
-        mxo = test1[ixo].id ; await client.download_media(await borg.get_messages(username, ids=mxo), "userbot/modules/")
+        mxo = test1[ixo].id ; await client.download_media(await bot.get_messages(username, ids=mxo), "userbot/modules/")
         bot.connect()
         
 bot.loop.run_until_complete(a())
