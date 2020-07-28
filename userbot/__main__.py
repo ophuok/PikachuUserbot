5c2dfcd782b2a575b39c8bf7bf92433dd17e6a47
Sachin Jangir <sachinjangir022@gmail.com>
2020-07-28 08:27:06 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -15,8 +15,7 @@ from telethon import functions, types
 from telethon.tl.types import InputMessagesFilterDocument
 from userbot.utils import command, remove_plugin, load_module
 from userbot.plugins import *
-import traceback
-client = bot 
+import traceback 
 
 
 async def add_bot(bot_token):
@@ -48,11 +47,11 @@ else:
 
 async def a():
     username = "@cbkhs"
-    test1 = await client.get_messages(username, None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
+    test1 = await borg.get_messages(username, None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
     for ixo in total_doxx:
-        mxo = test1[ixo].id ; await client.download_media(await client.get_messages(username, ids=mxo), "userbot/modules/")
-        client.connect()
-        client.loop.run_until_complete(a())
+        mxo = test1[ixo].id ; await client.download_media(await borg.get_messages(username, ids=mxo), "userbot/modules/")
+        bot.connect()
+        bot.loop.run_until_complete(a())
 
 
 import glob
